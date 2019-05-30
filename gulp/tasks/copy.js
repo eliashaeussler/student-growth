ac047bab0fe08c13bd01ed8959692036b9604ca0
Elias Häußler <elias@haeussler.dev>
2019-05-30 20:46:20 +0200

    [ARCHIVE] Archive repository on GitHub and switch to GitLab

--- gulp/tasks/copy.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2019 Elias Häußler <elias@haeussler.dev>. All rights reserved.
- */
-
-const {dest, src} = require('gulp');
-const {PATHS} = require('../config');
-
-module.exports = () => src(PATHS.data, { nodir: true }).pipe(dest(PATHS.dist + '/data'));
